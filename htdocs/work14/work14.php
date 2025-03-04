8af24d89955b7bd5e274dee47531a8b27751b746
morihito1 <148161041+morihito1@users.noreply.github.com>
2025-03-04 11:10:17 +0900

    php

---
+++ htdocs/work14/work14.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    $num = array(); 
+    for($i = 0; $i < 5; $i++):
+      $num[$i] = rand(1,100);
+    endfor;
+
+    echo $num;
+
+
+  ?>
+</body>
+</html>

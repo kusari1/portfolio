b46fbac1cc139f2959ccced9b34d81d17dfec36e
morihito1 <148161041+morihito1@users.noreply.github.com>
2025-03-06 14:59:35 +0900

    Update work21.php

--- htdocs/work21/work21.php
+++ htdocs/work21/work21.php
@@ -3,6 +3,10 @@
   if(isset($_POST['check_data'])){
     $check_data = htmlspecialchars($_POST['check_data'], ENT_QUOTES, 'UTF-8');
   }
+  $tell ='';		// 初期化
+  if(isset($_POST['tell'])){
+    $tell = htmlspecialchars($_POST['tell'], ENT_QUOTES, 'UTF-8');
+  }
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,6 +20,11 @@
             <input type="text" name="check_data" value= <?php echo $check_data ?>>
             <input type="submit" value="送信">
         </form>
+        <form method="POST">
+          <div>携帯電話番号を入力してください。</div>
+          <input type="tel" name="tell">
+          <input type="submit" value="送信">
+        </form>
         <?php
             if (!preg_match("/^[Ａ-Ｚ0-9０-９]+$/", $check_data) && $check_data !== '') {
                 if(preg_match("/dc/", $check_data)){
@@ -26,6 +35,10 @@
                 }
             }else{
               echo "正しい入力形式ではありません";
+            }
+
+            if(!preg_match("/^090|080|070-[0-9]{2,4}-[0-9]{3,4}$/",$tell)){
+              echo "携帯電話番号の形式ではありません";
             }
         ?>
     </body>
